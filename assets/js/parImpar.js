b1ff4275374b8cf45c89a3f10a8c856baa179bb3
maxigonza10 <maximo401777@gmail.com>
2024-05-03 10:31:39 -0300

    se termino el segundo punto

---
+++ assets/js/parImpar.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt('Ingrese un numero'))
+if (numero % 2 === 0){
+    console.log('El numero ' + numero + ' es par')
+}else{
+    console.log('El numero ' + numero + ' es impar')
+}
