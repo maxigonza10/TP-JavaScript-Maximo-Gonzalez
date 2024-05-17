8694a5bed9abb241d49b8bcba8773ffb08819524
maxigonza10 <maximo401777@gmail.com>
2024-05-17 10:37:56 -0300

    se declaro una variable

---
+++ Trabajo_Practico_3/assets/js/app.js
@@ -0,0 +1,9 @@
+
+while (opcion =! 0){
+    let opcion = parseInt(prompt(" 1. Agregar tarea\n 2. Mostrar lista \n 3. Editar tarea \n 4. Eliminar tarea \n 0. Terminar con el bucle"));
+    if (opcion = 0){
+        console.log("salir")
+    }
+}
+
+
